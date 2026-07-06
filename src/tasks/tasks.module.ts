f6268c4eb32a2316d153a55f39b824ab08a39e25
vlasenko-m <@email.com>
2026-07-06 14:27:58 +0200

    add lables to the task

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { TaskEntity } from './task.entity';
+import { TaskEntity } from './entitys/task.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskLabelEntity } from './entitys/task-label.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskEntity])],
+  imports: [TypeOrmModule.forFeature([TaskEntity , TaskLabelEntity])],
   controllers: [TasksController],
   providers: [TasksService],
 })
